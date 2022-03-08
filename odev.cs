06433fa0bee325f1309fc1c86e491ec91d8a76ca
Erkut Ateş <100518509+FireChief1@users.noreply.github.com>
2022-03-08 20:12:58 +0300

    Create odev.cs

---
+++ odev.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
